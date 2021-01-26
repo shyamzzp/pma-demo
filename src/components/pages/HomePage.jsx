030bc7235700e5ff42e1c97a6b08f20238c80d76
shyamzzp <shyamsundersuthar009@gmail.com>
2021-01-27 02:13:43 +0530

    Remove the unused components and code from the project.

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -4,75 +4,23 @@ import {
     Navbar,
     NavLeft,
     NavTitle,
-    NavRight,
     Link,
-    Toolbar,
-    Block,
-    BlockTitle,
     List,
-    ListItem,
-    Row,
-    Col,
-    Button
-} from 'framework7-react';
+    ListItem} from 'framework7-react';
 
 export default ({ 
   onGoToAbout,
-  onGoToForm,
-  onGoToDynamicRoute,
-  onGoToDefaultRoute,
   onOpenLeftPanel,
-  onOpenRightPanel,
-  onOpenPopup,
-  onOpenLoginScreen
 }) => (
   <Page>
     <Navbar>
       <NavLeft>
         <Link iconIos="f7:menu" iconMd="material:menu" link onClick={onOpenLeftPanel}></Link>
       </NavLeft>
-      <NavTitle>My App</NavTitle>
-      <NavRight>
-        <Link iconIos="f7:menu" iconMd="material:menu" link onClick={onOpenRightPanel}></Link>
-      </NavRight>
+      <NavTitle>People May Ask</NavTitle>
     </Navbar>
-    <Toolbar>
-      <Link>Left Link</Link>
-      <Link>Right Link</Link>
-    </Toolbar>
-    <Block strong>
-      <p>Here is your blank Framework7 app. Let's see what we have here.</p>
-    </Block>
-    <BlockTitle>Navigation</BlockTitle>
     <List>
       <ListItem link onClick={onGoToAbout} title="About"></ListItem>
-      <ListItem link onClick={onGoToForm} title="Form"></ListItem>
-    </List>
-    <BlockTitle>Modals</BlockTitle>
-    <Block strong>
-      <Row>
-        <Col width="50">
-          <Button fill raised onClick={onOpenPopup}>Popup</Button>
-        </Col>
-        <Col width="50">
-          <Button fill raised onClick={onOpenLoginScreen}>Login Screen</Button>
-        </Col>
-      </Row>
-    </Block>
-    <BlockTitle>Panels</BlockTitle>
-    <Block strong>
-      <Row>
-        <Col width="50">
-          <Button fill raised onClick={onOpenLeftPanel}>Left Panel</Button>
-        </Col>
-        <Col width="50">
-          <Button fill raised onClick={onOpenRightPanel}>Right Panel</Button>
-        </Col>
-      </Row>
-    </Block>
-    <List>
-      <ListItem link onClick={onGoToDynamicRoute} title="Dynamic Route"></ListItem>
-      <ListItem link onClick={onGoToDefaultRoute} title="Default Route (404)"></ListItem>
     </List>
   </Page>
 );
